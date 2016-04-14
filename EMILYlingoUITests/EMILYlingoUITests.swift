ba4069c619561c2934b1feffd6a34b27dc179a32
kus.jacob <kus.jacob@imac-d25hm0pzdhjw.cs.tamu.edu>
2016-04-14 15:12:37 -0500

    Added Test UI Cases

--- EMILYlingoUITests/EMILYlingoUITests.swift
+++ EMILYlingoUITests/EMILYlingoUITests.swift
@@ -28,9 +28,117 @@ class EMILYlingoUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRecord() {
+        let app = XCUIApplication()
+        
+        sleep(2)
+        
+        //Lead to Recording screen
+        let toRecordScreenButton = app.navigationBars["EMILYlingo.PhrasesView"].buttons["Item"]
+        toRecordScreenButton.tap()
+        
+        //Start Recording, wait for 5 seconds
+        sleep(2)
+        let recordButtonStart = app.buttons["RecordOff"]
+        recordButtonStart.tap()
+        
+        sleep(5)
+        
+        //Stop the Recording, leading to save screen
+        let recordButtonStop = app.buttons["RecordingOn"]
+        recordButtonStop.tap()
+        sleep(2)
+        
+        //Once Recording is done, agree to save by 'Save' Button
+        let agreeButton = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        agreeButton.tap()
+        if(agreeButton.childrenMatchingType(.Button)["Save"].exists)
+        {
+            agreeButton.childrenMatchingType(.Button)["Save"].tap()
+        }
+        else if(agreeButton.childrenMatchingType(.Button)["abroar"].exists)
+        {
+            agreeButton.childrenMatchingType(.Button)["abroar"].tap()
+        }
+        
+        sleep(2)
+        
+        let fieldBox = agreeButton.childrenMatchingType(.Other).elementBoundByIndex(1)
+        fieldBox.childrenMatchingType(.TextField).elementBoundByIndex(0).tap()
+        fieldBox.childrenMatchingType(.TextField).elementBoundByIndex(0).typeText("yoyoyo")
+        
+        let textField = fieldBox.childrenMatchingType(.TextField).elementBoundByIndex(1)
+        app.buttons["👩"].tap()
+        textField.tap()
+        //textField.tap()
+        fieldBox.childrenMatchingType(.TextField).elementBoundByIndex(1).typeText("Polish")
+        //Debug fails to enter language.
+        //app.buttons["👩"].tap()
+        fieldBox.buttons["Save"].tap()
+        
+        sleep(2)
     }
+    
+    func testSwapLanguage() {
+        
+        let app = XCUIApplication()
+        
+        
+        let buttonLanguage = app.navigationBars["EMILYlingo.PhrasesView"].childrenMatchingType(.Button).elementBoundByIndex(0)
+        sleep(1)
+        buttonLanguage.tap()
+        app.buttons["English"].tap()
+        sleep(1)
+        buttonLanguage.tap()
+        app.buttons["Spanish"].tap()
+        sleep(1)
+        buttonLanguage.tap()
+        app.buttons["Arabic"].tap()
+        sleep(1)
+        buttonLanguage.tap()
+        app.buttons["English"].tap()
+        sleep(1)
+    }
+    
+    func testRecordReject() {
+        
+        let app = XCUIApplication()
+        
+        //To Recording Screen
+        app.navigationBars["EMILYlingo.PhrasesView"].buttons["Item"].tap()
+        sleep(1)
+        
+        //Set Recording
+        app.buttons["RecordOff"].tap()
+        sleep(3)
+        app.buttons["RecordingOn"].tap()
+        
+        //Choose to save
+        let agreeButton = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        agreeButton.childrenMatchingType(.Button)["Save"].tap()
+        sleep(1)
+        
+        //Reject and Head Back
+        agreeButton.childrenMatchingType(.Other).elementBoundByIndex(1).buttons["Cancel"].tap()
+        sleep(1)
+        app.navigationBars["EMILYlingo.View"].buttons["-"].tap()
+        sleep(2)
+        
+    }
+    
+    func testSetSort() {
+        
+        let app = XCUIApplication()
+        let buttonSort = app.navigationBars["EMILYlingo.PhrasesView"].childrenMatchingType(.Button).elementBoundByIndex(0)
+        sleep(2)
+        buttonSort.tap()
+        app.buttons["Alphabetically"].tap()
+        sleep(2)
+        buttonSort.tap()
+        app.buttons["By Date"].tap()
+        sleep(2)
+        
+    }
+
     
 }
